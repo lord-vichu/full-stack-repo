fa0507f3c4323f43aedc67533c15c458287b651a
lord-vichu <adhithvichu@gmail.com>
2026-01-14 11:38:52 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,15 @@
+const additem=()=>{
+    //Step1. Read user input from textbox
+    let userinput=document.querySelector("#inp").value
+    //Step2. Create list item
+    let listitem=document.createElement("li")
+    listitem.textContent=userinput
+    //step3. Target the list
+    let list=document.querySelector("#todo")
+    //Step4. Add item to the list
+    list.appendChild(listitem)
+  
+}
+let btn=document.querySelector("#btn")
+//document.getElemntById("btn");
+btn.addEventListener("click",additem)
